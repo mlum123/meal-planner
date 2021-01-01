4c5b3c322722a159976c3ddf60cf97cfa8c83245
mlum123 <michellelum123@gmail.com>
2020-12-31 16:54:53 -0800

    made schedule page responsive

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -4,11 +4,88 @@ import "./Schedule.css";
 import MealCard from "./MealCard";
 
 class Schedule extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { smallScreen: false };
+
+    this.updateScreenSize = this.updateScreenSize.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateScreenSize();
+    window.addEventListener("resize", this.updateScreenSize);
+  }
+
+  componentWillUnmount() {
+    this.updateScreenSize();
+    window.removeEventListener("resize", this.updateScreenSize);
+  }
+
+  updateScreenSize() {
+    this.setState({ smallScreen: window.innerWidth < 992 });
+  }
+
   render() {
     let days = ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"];
     let meals = ["Breakfast", "Lunch", "Dinner"];
 
-    return (
+    // display only 1 col in each row if screen size < 992px; otherwise, display 3-col table
+    return this.state.smallScreen ? (
+      <Container className="Schedule mt-3">
+        {days.map((day) => {
+          return (
+            <>
+              <Row key={day}>
+                <Col className="table-head text-center">{day}</Col>
+              </Row>
+              <hr></hr>
+              {meals.map((meal) => {
+                let lowerCaseMeal = meal.toLowerCase();
+                return (
+                  <>
+                    <Row className="align-items-center">
+                      <div className="table-head">{lowerCaseMeal}</div>
+                      <Col
+                        className="offset-2 td-parent"
+                        key={day + lowerCaseMeal}
+                      >
+                        {this.props.meals[day][lowerCaseMeal] &&
+                        (this.props.meals[day][lowerCaseMeal]
+                          .analyzedInstructions ||
+                          this.props.meals[day][lowerCaseMeal]
+                            .readyInMinutes) ? (
+                          <MealCard
+                            recipe={this.props.meals[day][lowerCaseMeal]}
+                            key={this.props.meals[day][lowerCaseMeal].id}
+                            isRemoval={true}
+                            onRemove={this.props.onRemove}
+                            day={day}
+                            time={lowerCaseMeal}
+                          />
+                        ) : null}
+                        {this.props.meals[day][lowerCaseMeal] &&
+                        (this.props.meals[day][lowerCaseMeal].location ||
+                          this.props.meals[day][lowerCaseMeal].price) ? (
+                          <MealCard
+                            restaurant={this.props.meals[day][lowerCaseMeal]}
+                            key={this.props.meals[day][lowerCaseMeal].id}
+                            isRemoval={true}
+                            onRemove={this.props.onRemove}
+                            day={day}
+                            time={lowerCaseMeal}
+                          />
+                        ) : null}
+                      </Col>{" "}
+                    </Row>
+                    <br></br>
+                  </>
+                );
+              })}
+            </>
+          );
+        })}
+      </Container>
+    ) : (
       <Container className="Schedule mt-3">
         <Row>
           <Col className="xs-4 table-head text-center">Breakfast</Col>
